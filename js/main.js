7e799d7cf370d12c25a7ae5c736dd4340117b066
Emmanuel-Angeles <yeangels1595@gmail.com>
2022-11-09 00:33:15 -0600

    Feat: Metodo uno para activar el tab button

--- js/main.js
+++ js/main.js
@@ -6,10 +6,5 @@ tabsButton.forEach(tabButton => {
             tabButton.classList.remove("active_tab_button")
         })
         tabButton.classList.add("active_tab_button")
-        // let current = document.getElementsByClassName("active_tab_button");
-        // if (current.length > 0) { 
-        //     current[0].classList.remove("active_tab_button");
-        // }
-        // this.classList.add("active_tab_button")
     }
 });
