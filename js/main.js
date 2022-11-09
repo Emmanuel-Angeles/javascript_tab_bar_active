e1f4caa0ca739fc472e5b967ba06784ee0434d8a
Emmanuel-Angeles <yeangels1595@gmail.com>
2022-11-09 00:15:36 -0600

    Feat: Creacion de tab bar estilo mobile y agregar clase active a tab clickeada utilizando efectos y transiciones

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const tabsButton = document.querySelectorAll(".tab_button");
+
+tabsButton.forEach(tabButton => {
+    tabButton.onclick = function () {
+        tabsButton.forEach(tabButton => {
+            tabButton.classList.remove("active_tab_button")
+        })
+        tabButton.classList.add("active_tab_button")
+        // let current = document.getElementsByClassName("active_tab_button");
+        // if (current.length > 0) { 
+        //     current[0].classList.remove("active_tab_button");
+        // }
+        // this.classList.add("active_tab_button")
+    }
+});
